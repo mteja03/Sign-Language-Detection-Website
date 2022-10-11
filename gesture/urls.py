016491ca647af4e72214ef1828b50f51071a29e5
mteja03 <62012634+mteja03@users.noreply.github.com>
2022-10-11 14:33:41 +0100

    Update urls.py

--- gesture/urls.py
+++ gesture/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.views.static import serve
+from django.conf.urls import urls
+
 urlpatterns = [
     path('',include('findproj.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
